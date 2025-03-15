82e7148d8e70d48cb7457b406fcb07f006526f35
Elliot Chan <elliot.chan@student.tdsb.on.ca>
2025-03-15 02:25:24 +0000

    Finish practice problem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,28 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static boolean validIndex(int[] array, int num) {
+		try {
+			array[num] = 0;
+			return true;
+		}catch(ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+	public static int divide(int num1, int num2) {
+		try {
+			return num1 / num2;
+		}catch(Exception e) {
+			return 0;
+		}
+	}
+	public static int safeConvertStringtoInt(String s) {
+		try {
+			return Integer.parseInt(s);
+		}catch(Exception e) {
+			return 0;
+		}
+
+	}
 
 }
